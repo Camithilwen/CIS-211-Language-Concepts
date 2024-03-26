cc8a90b702bf099919df52062d8868112572ba6b
jamieruth_3 <noduckwiththat@duck.com>
2024-03-26 19:43:26 -0400

    begin work on chapter 12 content

---
+++ Ch_12_Assets/Card.java
@@ -0,0 +1,38 @@
+
+/**
+ * Class defining a single playing card,taken from Chapter 12 of
+ * the ThinkJava textbook.
+ *
+ * @author Jamie Kemman
+ * @version 1.0
+ */
+public class Card
+{
+    //instance variables
+    private int rank;
+    private int suit;
+    
+    /**
+     * Constructor for a Card object. This represnts a singular playing card
+     * from a traditional deck.
+     * @param takes an integer encoding for rank and an integer encoding for suit.
+     * @param Encoding is described in the Constructor documentation.
+     * 
+     * Encoding is as follows:
+     * 
+     * Rank
+     *  Ace -> 1
+     *  . . .
+     *  King -> 13
+     * 
+     * Suit
+     *  Clubs -> 1
+     *  Diamonds -> 2
+     *  Hearts -> 3
+     *  Spades -> 4
+     */
+    public Card(int rank, int suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+}
